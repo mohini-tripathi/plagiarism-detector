d106ae854ed2f7f635271e5b84fe6107ae8618d8
MOHINI TRIPATHI <trishmohi01@gmail.com>
2019-12-07 22:14:59 +0530

    first commit

---
+++ plagiarism.py
@@ -0,0 +1,92 @@
+from flask import Flask, request, render_template
+import re  #regular expression
+import math
+
+app = Flask("__name__") #Flask constructor takes the name of current module (__name__) as argument.
+
+q = ""
+
+@app.route("/") # displays the index page accessible at '/' & tells the application which URL should call the associated function.
+def loadPage():
+	return render_template('index.html', query="")
+
+
+@app.route("/", methods=['POST'])
+def cosineSimilarity():
+	
+	universalSetOfUniqueWords = []
+	matchPercentage = 0
+
+	####################################################################################################
+	
+	inputQuery = request.form['query']
+	lowercaseQuery = inputQuery.lower()
+
+	queryWordList = re.sub("[^\w]", " ",lowercaseQuery).split()			#Replace punctuation by space and split
+	# queryWordList = map(str, queryWordList)	 re.sub(pattern,repl,string)				#This was causing divide by zero error
+
+	for word in queryWordList:
+		if word not in universalSetOfUniqueWords:
+			universalSetOfUniqueWords.append(word)
+
+	####################################################################################################
+
+	fd = open("database1.txt", "r")
+	database1 = fd.read().lower()
+
+	databaseWordList = re.sub("[^\w]", " ",database1).split()	#Replace punctuation by space and split
+	# databaseWordList = map(str, databaseWordList)			#And this also leads to divide by zero error
+
+	for word in databaseWordList:
+		if word not in universalSetOfUniqueWords:
+			universalSetOfUniqueWords.append(word)
+
+	####################################################################################################
+
+	queryTF = []
+	databaseTF = []
+
+	for word in universalSetOfUniqueWords:
+		queryTfCounter = 0
+		databaseTfCounter = 0
+
+		for word2 in queryWordList:
+			if word == word2:
+				queryTfCounter += 1
+		queryTF.append(queryTfCounter)
+
+		for word2 in databaseWordList:
+			if word == word2:
+				databaseTfCounter += 1
+		databaseTF.append(databaseTfCounter)
+
+	dotProduct = 0
+	for i in range (len(queryTF)):
+		dotProduct += queryTF[i]*databaseTF[i]
+
+	queryVectorMagnitude = 0
+	for i in range (len(queryTF)):
+		queryVectorMagnitude += queryTF[i]**2
+	queryVectorMagnitude = math.sqrt(queryVectorMagnitude)
+
+	databaseVectorMagnitude = 0
+	for i in range (len(databaseTF)):
+		databaseVectorMagnitude += databaseTF[i]**2
+	databaseVectorMagnitude = math.sqrt(databaseVectorMagnitude)
+
+	matchPercentage = (float)(dotProduct / (queryVectorMagnitude * databaseVectorMagnitude))*100
+
+	result = "Input query text matches %0.02f%% with database."%matchPercentage
+
+	return render_template('index.html', query=inputQuery, result=result)
+app.debug = True
+app.run() # four arguements : host, port, debug, options
+app.run(debug = True)
+
+
+# Let :
+#     a : [x1, y1, z1]
+#     b : [x2, y2, z2]
+
+# (Theta) = (x1*x2 + y1*y2 + z1*z2)/
+#          sqrt((x1^2 + y1^2 + z1^2))*sqrt((x2^2 + y2^2 + z2^2))
---
+++ test_cases.py
@@ -0,0 +1,7 @@
+import plagiarism
+def test_a():
+    assert sum
+
+if __name__ == "__main__":
+    test_a()
+    print("Everything passed")
